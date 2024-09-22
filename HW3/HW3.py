9c073afd319a0249fa2bb68752edc2ad2ba947f2
TimofeiKazakov <violet09502@gmail.com>
2024-09-22 11:34:24 +0300

    Add files via upload

---
+++ HW3/HW3.py
@@ -0,0 +1,6 @@
+homework_count = 12
+time_count = 1+1/2
+average_execution_time = (time_count / homework_count)
+name = "Python"
+print(time_count / homework_count)
+print(name, homework_count, time_count, average_execution_time)
